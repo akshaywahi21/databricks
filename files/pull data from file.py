6741a03ea246c2c096c3881d163d8b4b6c714304
akshaywahi21 <akshaywahi21>
2025-01-08 21:54:17 +0000

    read file code

--- files/pull data from file.py
+++ files/pull data from file.py
@@ -1,3 +1,12 @@
 # Databricks notebook source
 print('hello')
 print(1+7)
+
+# COMMAND ----------
+
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("MyApp").getOrCreate()
+
+df = spark.read.csv("file:/Workspace/Users/akshaywahi@gmail.com/files/industry.csv", header=True, inferSchema=True)
+display(df)
